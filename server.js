21057a9d68c35458f43d93b4e59bec8ce31df14e
Julio-Vasquez <jualvalitube@gmail.com>
2021-10-26 23:00:35 -0500

    feat(http-errors): handle http errors

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const app = require('express')(),
+const createError = require('http-errors'),
+  app = require('express')(),
   cors = require('cors')
 
 const PORT = 8550
 
 app.use(cors())
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.send({ status: err.status || 500, message: err.message })
+})
 
 app.use('/', require('./src/routes/app.routes'))
 
